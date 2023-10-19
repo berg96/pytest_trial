bbbc454f383a32a55eee63e63c4449982a294846
Artem Kulikov <chigar2010@yandex.ru>
2023-10-19 23:57:56 +0200

    init, add tests, trial, commit, push, sleep

---
+++ test_exp.py
@@ -0,0 +1,41 @@
+from time import sleep
+
+import pytest
+
+
+def one_more(x):
+    return x + 1
+
+
+def get_sort_list(str):
+    new_list = sorted(str.split(', '))
+    return new_list
+
+
+@pytest.mark.parametrize(
+    'input_arg, expected_result',  # Названия аргументов, передаваемых в тест.
+    [(4, 5), (3, 5)]  # Список кортежей со значениями аргументов.
+)
+def test_one_more(input_arg, expected_result):  # Те же параметры, что и в декораторе.
+    assert one_more(input_arg) == expected_result
+
+
+def test_sort():
+    """Тестируем функцию get_sort_list()."""
+    result = get_sort_list('Яша, Саша, Маша, Даша')
+    assert result == ['Даша', 'Маша', 'Саша', 'Яша']
+
+
+@pytest.mark.slow
+def test_type():
+    """Тестируем тип данных, возвращаемых из get_sort_list()."""
+    # Провальный тест:
+    # ожидаем число, но вернётся список.
+    sleep(3)
+    result = get_sort_list('Яша, Саша, Маша, Даша')
+    assert isinstance(result, int)
+
+
+@pytest.mark.xfail(reason='Пусть пока падает, завтра починю.')
+def test_false():
+    assert False
